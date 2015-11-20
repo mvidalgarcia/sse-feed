8a5ee782985c9e5526f860b9f6f104bc38557415
Marco Vidal García <marcovidal67@gmail.com>
2015-11-20 15:03:40 +0100

    Two kind of events

--- js/main.js
+++ js/main.js
@@ -1,14 +1,34 @@
 var source = new EventSource('server/events.php')
 
-source.addEventListener('message', function(e) {
-  if (e.origin != 'http://localhost') {
-    alert('Origin was not http://localhost');
-    return;
+var eventsSubscribed = {
+  "police": true,
+  "firefighter": false,
+}
+
+
+$.each(eventsSubscribed, function(eventType, subscribed) {
+  if (subscribed) {
+    source.addEventListener(eventType, function(e) {
+      if (e.origin != 'http://localhost') {
+        alert('Origin was not http://localhost')
+        return
+      }
+      var obj = JSON.parse(e.data);
+      addFeedItem(obj.info + " at " + obj.time)
+    }, false)
   }
-  console.log(e.data)
-  addFeedItem(e.data)
-}, false)
+  else {
+    console.info("Not subscribed to " + eventType + " events.")
+  }
+
+})
+
 
 function addFeedItem(data) {
   $("#list").append("<li>" + data + "</li>")
 }
+
+
+$(function() {
+
+})
--- server/events.php
+++ server/events.php
@@ -1,6 +1,14 @@
 <?php
+date_default_timezone_set("Europe/Madrid");
 header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache'); // recommended to prevent caching of event data.
+
+$arrayEvents = array(
+  "police" => "ISIS attack in Paris Le Bataclan",
+  "firefighter" => "Fire in Madrid El Retiro",
+);
+
+$rand = rand(0, count($arrayEvents)-1);
 
 /**
  * Constructs the SSE data format and flushes that data to the client.
@@ -8,14 +16,17 @@ header('Cache-Control: no-cache'); // recommended to prevent caching of event da
  * @param string $id Timestamp/id of this connection.
  * @param string $msg Line of text that should be transmitted.
  */
-function sendMsg($id, $msg) {
+function sendMsg($event, $id, $msg) {
+  global $arrayEvents;
+  echo "event: $event" . PHP_EOL;
   echo "id: $id" . PHP_EOL;
-  echo "data: $msg" . PHP_EOL;
+  echo "data: {{$msg}, \"info\": \"$arrayEvents[$event]\"}" . PHP_EOL;
   echo PHP_EOL;
   ob_flush();
   flush();
 }
 
+$event = array_keys($arrayEvents)[$rand];
 $serverTime = time();
 
-@sendMsg($serverTime, 'server time: ' . date("h:i:s", time()));
+sendMsg($event, $serverTime, '"time": ' . '"' . date("H:i:s", time()) . '"' );
