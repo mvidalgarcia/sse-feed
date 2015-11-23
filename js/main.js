8a6fb861b4ebd6a3bbee37aad21dec99e4c04819
Marco Vidal García <marcovidal67@gmail.com>
2015-11-23 14:05:42 +0100

    Fetching alerts from two sources
    Combo boxes to choose event subscriptions

--- js/main.js
+++ js/main.js
@@ -1,27 +1,48 @@
-var source = new EventSource('server/events.php')
-
-var eventsSubscribed = {
-  "police": true,
-  "firefighter": false,
+var myFeeds = {
+  mine: {
+    url: "server/events.php",
+    eventSource: null
+  },
+  david: {
+    url: "http://156.35.95.69/server-sent-events/server/server.php",
+    eventSource: null
+  }
 }
 
 
-$.each(eventsSubscribed, function(eventType, subscribed) {
-  if (subscribed) {
-    source.addEventListener(eventType, function(e) {
-      if (e.origin != 'http://localhost') {
-        alert('Origin was not http://localhost')
-        return
-      }
-      var obj = JSON.parse(e.data);
-      addFeedItem(obj.info + " at " + obj.time)
-    }, false)
+function handleEventSubscription(e) {
+  if (e.checked)
+    newSubscription(e.name)
+  else
+    closeSubscription(e.name)
+}
+
+
+function newSubscription(name) {
+  myFeeds[name].eventSource = new EventSource(myFeeds[name].url)
+  var source = myFeeds[name].eventSource
+
+  source.addEventListener('message', function(e) {
+    if (e.origin != 'http://localhost' && e.origin != 'http://156.35.95.69') {
+      alert('Danger! Origin ' + e.origin + ' unknown!');
+      return
+    }
+    console.log(e.data)
+    addFeedItem(e.data)
+  }, false)
+  console.info("New event subscription with '" + name + "' source.")
+}
+
+
+function closeSubscription(name) {
+  if (myFeeds[name].eventSource) {
+    myFeeds[name].eventSource.close()
+    console.info("Event source '" + name + "' was closed.")
   }
   else {
-    console.info("Not subscribed to " + eventType + " events.")
+    console.info("Couldn't close event source '" + name + "' :(")
   }
-
-})
+}
 
 
 function addFeedItem(data) {
--- server/events.php
+++ server/events.php
@@ -1,32 +1,52 @@
 <?php
 date_default_timezone_set("Europe/Madrid");
-header('Content-Type: text/event-stream');
+header("Content-Type: text/event-stream\n\n");
 header('Cache-Control: no-cache'); // recommended to prevent caching of event data.
 
+$TIME_BETWEEN_EVENTS = 20; // in seconds
+
 $arrayEvents = array(
-  "police" => "ISIS attack in Paris Le Bataclan",
-  "firefighter" => "Fire in Madrid El Retiro",
+  0 => array(
+    "message" => "ISIS attack in Paris Le Bataclan",
+    "alertLevel" => "high",
+    "source" => "French Police",
+    "sourceIcon" => "photo128x128",
+  ),
+  1 => array(
+    "message" => "Terrorist threat of bomb in Madrid city center",
+    "alertLevel" => "medium",
+    "source" => "Spanish Police",
+    "sourceIcon" => "photo128x128",
+  ),
+  2 => array(
+    "message" => "Podemos huge demonstration against Government in Puerta del Sol",
+    "alertLevel" => "low",
+    "source" => "Spanish Police",
+    "sourceIcon" => "photo128x128",
+  )
 );
 
-$rand = rand(0, count($arrayEvents)-1);
 
-/**
- * Constructs the SSE data format and flushes that data to the client.
- *
- * @param string $id Timestamp/id of this connection.
- * @param string $msg Line of text that should be transmitted.
- */
-function sendMsg($event, $id, $msg) {
+function sendMsg($time, $msg) {
   global $arrayEvents;
-  echo "event: $event" . PHP_EOL;
-  echo "id: $id" . PHP_EOL;
-  echo "data: {{$msg}, \"info\": \"$arrayEvents[$event]\"}" . PHP_EOL;
+  $msg['timestamp'] = $time;
+  echo "data: " . json_encode($msg) . PHP_EOL;
   echo PHP_EOL;
   ob_flush();
   flush();
 }
 
-$event = array_keys($arrayEvents)[$rand];
-$serverTime = time();
 
-sendMsg($event, $serverTime, '"time": ' . '"' . date("H:i:s", time()) . '"' );
+function run() {
+  global $arrayEvents, $TIME_BETWEEN_EVENTS;
+  while(1) {
+    $serverTime = time();
+    $randIdx = rand(0, count($arrayEvents)-1);
+    sendMsg($serverTime, $arrayEvents[$randIdx]);
+    sleep($TIME_BETWEEN_EVENTS);
+  }
+}
+
+run();
+
+?>
